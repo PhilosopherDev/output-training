f1de18983abc4366070990fca40f8bf5db197605
PhilosopherProgrammer <nekarakube@gmail.com>
2021-06-12 12:16:27 +0900

    Primitive method examples

--- book/ThePrinciplesOfObjectOrientedJavascript/1.PrimitiveTypeAndRefferenceType/PrimitiveType.js
+++ book/ThePrinciplesOfObjectOrientedJavascript/1.PrimitiveTypeAndRefferenceType/PrimitiveType.js
@@ -24,3 +24,33 @@ console.log(typeof undefined); // undefined
  */
 console.log(typeof null); // object, 이래서 null 을 빈 object 라고 하는 경우가 있다. TC39 에서 실수라고 인정했다고 한다. 
 console.log(null); // null
+
+/**
+ * Primitive type method
+ * 원시 타입 메소드
+ * null 과 undefined 타입에는 메소드가 없다. 
+ */
+
+// String method
+var name = "Jayden";
+var lowercaseName = name.toLowerCase();
+console.log(lowercaseName); // jayden
+var firstLetter = name.charAt(0);
+console.log(firstLetter); // J
+var middleOfName = name.substring(2, 4);
+console.log(middleOfName); // yd
+
+// Number method
+var count = 10;
+var fixedCount = count.toFixed(2);
+console.log(fixedCount); // 10.00
+var hexCount = count.toString(16);
+console.log(hexCount); // a
+
+// Boolean method
+var flag = true;
+var stringFlag = flag.toString();
+console.log(stringFlag, typeof stringFlag); // "true", string
+
+
+
