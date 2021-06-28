e5af6f5e4c1287b9af61c6ddd83fe592583569e2
PhilosopherProgrammer <nekarakube@gmail.com>
2021-06-28 21:17:09 +0900

    My stack added

---
+++ book/DataStructureAndAlgorithm/MyDataStructure/MyStack.js
@@ -0,0 +1,43 @@
+/**
+ * Stack
+ * 
+ * push
+ * pop
+ * peek
+ * isEmpty
+ * clear
+ * size
+ */
+
+
+ var Stack = (function() {
+    function Stack() {
+        this._element = [];
+    }
+
+    Stack.prototype.push = function(item) {
+        this._element.push(item);
+    }
+
+    Stack.prototype.pop = function() {
+        return this._element.pop();
+    }
+
+    Stack.prototype.peek = function() {
+        return this._element[this._element.length];
+    }
+
+    Stack.prototype.isEmpty = function() {
+        return this._element.length === 0;
+    }
+
+    Stack.prototype.clear = function() {
+        this._element = [];
+    }
+
+    Stack.prototype.size = function() {
+        return this._element.length;
+    }
+
+    return Stack;
+})();
