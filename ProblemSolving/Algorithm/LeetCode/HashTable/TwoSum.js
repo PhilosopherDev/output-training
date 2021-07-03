2434772ec845b0b76847a3659b5e9788eca69105
PhilosopherProgrammer <nekarakube@gmail.com>
2021-07-03 22:56:42 +0900

    big O calculation

--- ProblemSolving/Algorithm/LeetCode/HashTable/TwoSum.js
+++ ProblemSolving/Algorithm/LeetCode/HashTable/TwoSum.js
@@ -6,17 +6,21 @@
  */
  var twoSum = function(nums, target) {
     var answer = [];
+    // O(n)
     var hashTable = nums.reduce((obj, num, idx) => {
         obj[num] = idx;
         return obj;
     }, {});
     
+    // O(n)
     for(let i=0; i < nums.length; i++) {
+        // O(1)
         if (hashTable[target - nums[i]] !== undefined && hashTable[target - nums[i]] !== i) {
             answer.push(i, hashTable[target - nums[i]]);
             break;
         }
     }
+    // O(n + n*1) => O(2n) => O(n)
     return answer;
 };
 
