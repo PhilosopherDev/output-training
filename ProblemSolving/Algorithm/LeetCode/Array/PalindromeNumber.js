2538c6c906889ab0ae82db10bf464b38d22eea63
PhilosopherProgrammer <nekarakube@gmail.com>
2021-07-04 22:14:13 +0900

    Palindrome Number - Array

---
+++ ProblemSolving/Algorithm/LeetCode/Array/PalindromeNumber.js
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/palindrome-number/
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false;
+    }    
+
+    if (x === Number.parseInt(x.toString().split("").reverse().join(""))) {
+        return true;
+    } else {
+        return false;
+    }
+};
