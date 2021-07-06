86bcd972d014f9912e9d5ec25505a32ffb636ba5
PhilosopherProgrammer <nekarakube@gmail.com>
2021-07-06 19:21:52 +0900

    Majority Element - HashTable

---
+++ ProblemSolving/Algorithm/LeetCode/HashTable/MajorityElement.js
@@ -0,0 +1,23 @@
+/**
+ * https://leetcode.com/problems/majority-element/
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var majorityElement = function(nums) {
+    var hashTable = {}, answer;
+    if (nums.length === 1) {
+        return nums[0];
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (hashTable[nums[i]] === undefined) {
+            hashTable[nums[i]] = 1;
+        } else {
+            hashTable[nums[i]]++;
+            if (hashTable[nums[i]] > (nums.length/2)) {
+                answer = nums[i];
+                break;
+            }
+        }
+    }
+    return answer;
+};
