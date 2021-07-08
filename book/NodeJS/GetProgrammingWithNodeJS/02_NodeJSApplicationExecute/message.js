6edc764406a6403b719825e5dcc57774aa7c5ee9
PhilosopherProgrammer <nekarakube@gmail.com>
2021-07-08 21:09:14 +0900

    using REPL example

---
+++ book/NodeJS/GetProgrammingWithNodeJS/02_NodeJSApplicationExecute/message.js
@@ -0,0 +1,24 @@
+let messages = [
+    "A change of environment can be a good thing!",
+    "You will make it!",
+    "Just run with the code!"
+];
+
+/**
+ * REPL
+
+> .load message.js
+let messages = [
+    "A change of environment can be a good thing!",
+        "You will make it!",
+            "Just run with the code!"
+            ];
+undefined
+> messages.forEach(message => console.log(message));
+A change of environment can be a good thing!
+You will make it!
+Just run with the code!
+undefined
+> .exit
+
+ */
