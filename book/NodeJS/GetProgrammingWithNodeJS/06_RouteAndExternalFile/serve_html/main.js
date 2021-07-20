f3102a72279fccf0508f80708b31256d9693cc3b
PhilosopherProgrammer <nekarakube@gmail.com>
2021-07-21 08:53:18 +0900

    serve_html by getViewUrl

--- book/NodeJS/GetProgrammingWithNodeJS/06_RouteAndExternalFile/serve_html/main.js
+++ book/NodeJS/GetProgrammingWithNodeJS/06_RouteAndExternalFile/serve_html/main.js
@@ -4,37 +4,83 @@
  */
 
 // my code 
-const port = 3000;
-http = require("http");
-httpStatus = require("http-status-codes");
-fs = require("fs");
+// const port = 3000;
+// http = require("http");
+// httpStatus = require("http-status-codes");
+// fs = require("fs");
 
-app = http.createServer((req, res) => {
-    console.log(req.url);
+// app = http.createServer((req, res) => {
+//     console.log(req.url);
     
-    const resMap = {
-        "/": function () {
-            fs.readFile("./views/index.html", "utf-8", (err, data) => {
-                if (err) {
-                    throw err;
-                }
-                res.end(data);
-            });
-        }
-    }
+//     const resMap = {
+//         "/": function () {
+//             fs.readFile("./views/index.html", "utf-8", (err, data) => {
+//                 if (err) {
+//                     throw err;
+//                 }
+//                 res.end(data);
+//             });
+//         }
+//     }
     
-    res.writeHeader(httpStatus.OK, {
-        "Content-Type": "text/html"
-    });
+//     res.writeHeader(httpStatus.OK, {
+//         "Content-Type": "text/html"
+//     });
     
-    if (resMap[req.url]) {
-        resMap[req.url]();
-    } else {
-        var resMessage = "<h1>Static message!</h1>";
-        res.end(resMessage);
-    }
+//     if (resMap[req.url]) {
+//         resMap[req.url]();
+//     } else {
+//         var resMessage = "<h1>Static message!</h1>";
+//         res.end(resMessage);
+//     }
     
     
-}).listen(port);
+// }).listen(port);
 
 // book code
+const port = 3000,
+    http = require("http"),
+    httpStatus = require("http-status-codes"),
+    fs = require("fs");
+
+// const routeMap = {
+//     "/": "views/index.html"
+// };
+// http.createServer((req, res) => {
+//     res.writeHead(httpStatus.OK, {
+//         "Content-Type": "text/html"
+//     });
+//     if (routeMap[req.url]) {
+//         fs.readFile(routeMap[req.url], (error, data) => {
+//             res.write(data);
+//             res.end();
+//         });
+//     } else {
+//         res.end("<h1>Sorry, not found.</h1>")
+//     }
+// })
+// .listen(port);
+// console.log(`The server has started and is listening on port number: ${port}`);
+
+//book code2
+const getViewUrl = (url) => {
+    return `views/${url}.html`;
+}
+
+http.createServer((req, res) => {
+    let viewUrl = getViewUrl(req.url);
+    fs.readFile(viewUrl, (err, data) => {
+        if (err) {
+            res.writeHead(httpStatus.NOT_FOUND);
+            res.write("<h1>FILE NOT FOUND</h1>");
+        } else {
+            res.writeHead(httpStatus.OK, {
+                "Content-Type": "text/html"
+            });
+            res.write(data);
+        }
+        res.end();
+    });
+})
+.listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
