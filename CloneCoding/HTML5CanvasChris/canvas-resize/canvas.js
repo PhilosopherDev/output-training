8c5046d5a2fb4e1338f36ee037cc569b1c7a8ef2
PhilosopherProgrammer <nekarakube@gmail.com>
2021-07-21 22:15:05 +0900

    Ep. 1 end

--- CloneCoding/HTML5CanvasChris/canvas-resize/canvas.js
+++ CloneCoding/HTML5CanvasChris/canvas-resize/canvas.js
@@ -3,3 +3,9 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext("2d");
+
+c.fillRect(100, 100, 100 ,100);
+c.fillRect(400, 100, 100 ,100);
+c.fillRect(300, 200, 100 ,100);
+
+console.log(canvas);
