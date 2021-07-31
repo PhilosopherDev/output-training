c8e1a01829ce355281a3bc87b7a5ff0cf2e1490d
PhilosopherProgrammer <nekarakube@gmail.com>
2021-07-31 22:47:09 +0900

    set up

--- CloneCoding/webpack-practice/webpack-demo/webpack.config.js
+++ CloneCoding/webpack-practice/webpack-demo/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'build.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
