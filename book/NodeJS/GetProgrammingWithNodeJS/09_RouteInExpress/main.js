58fa0277b4d54c0336aaa0ec6ac551ce0ba6f5e5
PhilosopherProgrammer <nekarakube@gmail.com>
2021-08-04 12:28:30 +0900

    req message analyze

--- book/NodeJS/GetProgrammingWithNodeJS/09_RouteInExpress/main.js
+++ book/NodeJS/GetProgrammingWithNodeJS/09_RouteInExpress/main.js
@@ -2,9 +2,25 @@ const port = 3000,
     express = require("express"),
     app = express();
 
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+app.use(express.json());
+
 app.use((req, res, next) => {
     console.log(`request made to ${req.url}`);
+    console.log(`request query ${req.query}`);
+    console.log(`request query stringify: ${JSON.stringify(req.query)}`);
     next();
+});
+
+app.post("/", (req, res) => {    
+    console.log("req.body", req.body);
+    console.log("req.body stringify", JSON.stringify(req.body));
+    console.log("req.query", req.query);
+    res.send("POST successful!");
 });
 
 app.get("/items/:vegetable", (req, res) => {
