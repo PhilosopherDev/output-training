158d7066f3fd1250976f788a6abc295c810c85c4
PhilosopherProgrammer <nekarakube@gmail.com>
2021-08-09 09:03:49 +0900

    capstone project2 init

---
+++ book/NodeJS/GetProgrammingWithNodeJS/12_CapstoneProject2/main.js
@@ -0,0 +1,17 @@
+const express = require("express"),
+    app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Confetti Cuisine!");
+});
+
+app.listen(app.get("port"), () => {
+    console.log(`Server running at http://localhost:${app.get("port")}`);
+});
