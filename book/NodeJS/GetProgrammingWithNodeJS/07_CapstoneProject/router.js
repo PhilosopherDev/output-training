84500f8feb28755636888f7d148a451bbc5c737a
PhilosopherProgrammer <nekarakube@gmail.com>
2021-08-09 22:21:15 +0900

    router refactor to singleton

--- book/NodeJS/GetProgrammingWithNodeJS/07_CapstoneProject/router.js
+++ book/NodeJS/GetProgrammingWithNodeJS/07_CapstoneProject/router.js
@@ -1,25 +1,63 @@
+"use strict";
+
 const httpStatus = require("http-status-codes"),
-    contentType = require("./content-types"),
-    utils = require("./utils");
+  contentTypes = require("./content-types"),
+  utils = require("./utils");
 
-const routes = {
-    "GET": {},
-    "POST": {}
-};
+// Router Singleton
+class Router {
+  constructor () {
+    this.routes = {
+      GET: {},
+      POST: {}
+    };
 
-exports.handle = (req, res) => {
-    try {
-        routes[req.method][req.url](req, res);
+    this.handle = this.handle.bind(this);
+    this.get = this.get.bind(this);
+    this.post = this.post.bind(this);
+  }
+  
+  handle(req, res) {
+    try {      
+      this.routes[req.method][req.url](req, res);
     } catch (e) {
-        res.writeHead(httpStatus.OK, contentType.html);
-        utils.getFile("views/error.html", res)
+      res.writeHead(httpStatus.OK, contentTypes.html);
+      utils.getFile("views/error.html", res);
     }
-};
+  };
 
-exports.get = (url, action) => {
-    routes["GET"][url] = action;
+  get(url, action) {
+    this.routes["GET"][url] = action;
+  };
+  
+  post(url, action) {
+    this.routes["POST"][url] = action;
+  };
 }
 
-exports.post = (url, action) => {
-    routes["POST"][url] = action;
-}
+module.exports = new Router();
+
+
+/** Legacy */
+
+// const routes = {
+//   GET: {},
+//   POST: {}
+// };
+
+// exports.handle = (req, res) => {
+//   try {
+//     routes[req.method][req.url](req, res);
+//   } catch (e) {
+//     res.writeHead(httpStatus.OK, contentTypes.html);
+//     utils.getFile("views/error.html", res);
+//   }
+// };
+
+// exports.get = (url, action) => {
+//   routes["GET"][url] = action;
+// };
+
+// exports.post = (url, action) => {
+//   routes["POST"][url] = action;
+// };
