f2f047f7ba521492f347397b93510c54d23a20ba
PhilosopherProgrammer <nekarakube@gmail.com>
2021-12-20 20:39:47 +0900

    array built-in func in anonymous func test

--- misc/Playground/anonymousFuncTest.js
+++ misc/Playground/anonymousFuncTest.js
@@ -1,4 +1,4 @@
-const arr = [1,2,3,4,5,6,7,8,9,10];
+const arr = [1,2,3,4,5,6,7,8,9];
 
 // arr.map((val) => {
 //     const uniqueId = Math.random();
@@ -26,13 +26,26 @@ const instanceTest = (function instanceTest() {
 arr.forEach(instanceTest);
 
 console.log("===============================");
-for (let i=1; i<11; i++) {
-    var instancetest2 = (() => {
+var instancetest2 = (() => {
+    const uniqueId = Math.random();
+    return function (val) {
+        console.log(val, uniqueId);
+        // console.log(this);
+    }
+})();
+
+for (let i=1; i<10; i++) {
+    instancetest2(i);
+}
+
+console.log("===============================");
+for (let i=1; i<10; i++) {
+    var instancetest3 = (() => {
         const uniqueId = Math.random();
         return function (val) {
             console.log(val, uniqueId);
             // console.log(this);
         }
     })();
-    instancetest2(i);
+    instancetest3(i);
 }
