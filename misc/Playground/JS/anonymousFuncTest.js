5578b141496fa7d9057e7be73ca2dbc759bf38bd
PhilosopherProgrammer <nekarakube@gmail.com>
2021-12-20 21:56:48 +0900

    anonymous func test example add

--- misc/Playground/JS/anonymousFuncTest.js
+++ misc/Playground/JS/anonymousFuncTest.js
@@ -49,3 +49,32 @@ for (let i=1; i<10; i++) {
     })();
     instancetest3(i);
 }
+
+console.log("===============================");
+
+function callbackTest(callback, idx) {
+    callback(idx);
+}
+
+var instancetest4 = (() => {
+    const uniqueId = Math.random();
+    return function (val) {
+        console.log(val, uniqueId);
+        // console.log(this);
+    }
+})();
+for (let i=1; i<10; i++) {
+    callbackTest(instancetest4, i);
+}
+
+console.log("===============================");
+for (let i=1; i<10; i++) {
+    var instancetest5 = (() => {
+        const uniqueId = Math.random();
+        return function (val) {
+            console.log(val, uniqueId);
+            // console.log(this);
+        }
+    })();
+    callbackTest(instancetest5, i);
+}
