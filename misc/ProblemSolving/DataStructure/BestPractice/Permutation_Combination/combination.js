c7dff475a2dfbf67451e512352ec554a6522a565
PhilosopherProgrammer <nekarakube@gmail.com>
2022-01-03 20:34:34 +0900

    combination best practice

---
+++ misc/ProblemSolving/DataStructure/BestPractice/Permutation_Combination/combination.js
@@ -0,0 +1,17 @@
+const getCombinations = (array, selectNumber) => {
+    const results = [];
+    if(selectNumber === 1){
+        return array.map((element) => [element]);
+    }
+    array.forEach((fixed, index, origin) => {
+        const rest = origin.slice(index+1);
+        const combinations = getCombinations(rest, selectNumber - 1);
+        const attached = combinations.map((combination) => [fixed, ...combination]);
+        results.push(...attached);
+    });
+    return results;
+}
+
+console.log(getCombinations(["1","2","3","4"], 3));
+console.log(getCombinations([1, 7], 1));
+console.log(getCombinations([1, 7], 2));
